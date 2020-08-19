1083b06385c6aa91ee760b132a4a540cf794a82e
Marc Podgorney <marc.podgorney@PBXUK-HH-00135.photobox.priv>
2020-08-19 13:47:00 +0100

    Updating example handler comments

--- src/handlers/exampleHandler.ts
+++ src/handlers/exampleHandler.ts
@@ -1,14 +1,14 @@
 /**
  * This is where you define your lamba function(s) that will interact with an 
- * incoming request/event in AWS. From here you perform various actions -
- * - push data to a pbx api
+ * incoming request/event in AWS (e.g. result of hitting an endpoint). From here you can perform various actions -
+ * - call into a pbx api
  * - listen on an sqs queue
  * - read/write data to an aws table (e.g. dynamo)
  */
-import { APIGatewayProxyResult } from 'aws-lambda'; //represents a lambda response
+import { APIGatewayProxyResult } from 'aws-lambda'; //represents a response from lambda
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
-  //extract data from request here
+  //extract data from request here etc.
 
   try {
     //e.g. attempt action (e.g. db read/write)

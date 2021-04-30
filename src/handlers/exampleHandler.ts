d013a8a61f7b952fa4d4807fdc8f20da24a0f328
Diana Baburina <dbaburina.consultant@newscorp.com>
2021-04-30 18:31:09 +0300

    Clean up example handler and use nodemon for reloading

--- src/handlers/exampleHandler.ts
+++ src/handlers/exampleHandler.ts
@@ -1,27 +1,8 @@
-/**
- * This is where you define your lamba function(s) that will interact with an 
- * incoming request/event in AWS (e.g. result of hitting an endpoint). 
- * Each lamba function is mapped to an endpoint via yaml config (in
- * '/serverless-partials/function.yml')
- * From within a lamba you can perform various actions:
- * - call into a pbx api
- * - listen on an sqs queue
- * - read/write data to an aws table (e.g. dynamo)
- */
-import { APIGatewayProxyResult } from 'aws-lambda'; //represents a response from lambda
+import { APIGatewayProxyResult } from 'aws-lambda';
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
-  //extract data from request here etc.
-
-  try {
-    //e.g. attempt action (e.g. db read/write)
-    const result: APIGatewayProxyResult = {
-      statusCode: 200,
-      body: 'this is a simple lambda handler',
-    };
-
-    return result;
-  } catch (error) {
-    //process/return any error
-  }
+export const handler = (): Promise<APIGatewayProxyResult> => {
+  return Promise.resolve({
+    statusCode: 200,
+    body: 'Hello from Photobox team',
+  });
 };
